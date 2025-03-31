1bc1eaae97626ed3895cacce61c6949c56bd3193
Jannik Buhr <jannik.m.buhr@gmail.com>
2025-03-31 14:50:14 +0200

    format

--- lua/cmp-pandoc-references/blink.lua
+++ lua/cmp-pandoc-references/blink.lua
@@ -2,7 +2,7 @@
 --- @module 'blink.cmp'
 --- @class blink.cmp.Source
 local source = {}
-local refs = require("cmp-pandoc-references.references")
+local refs = require 'cmp-pandoc-references.references'
 
 function source.new(opts)
   local self = setmetatable({}, { __index = source })
@@ -11,34 +11,34 @@ function source.new(opts)
 end
 
 function source:enabled()
-  return vim.o.filetype == "pandoc"
-    or vim.o.filetype == "markdown"
-    or vim.o.filetype == "rmd"
-    or vim.o.filetype == "quarto"
+  return vim.o.filetype == 'pandoc'
+    or vim.o.filetype == 'markdown'
+    or vim.o.filetype == 'rmd'
+    or vim.o.filetype == 'quarto'
 end
 
 function source:get_trigger_characters()
-  return { "@" }
+  return { '@' }
 end
 
 function source:get_completions(ctx, callback)
   local is_char_trigger =
     vim.list_contains(self:get_trigger_characters(), ctx.line:sub(ctx.bounds.start_col - 1, ctx.bounds.start_col - 1))
   if not is_char_trigger then
-    callback({
+    callback {
       items = {},
       is_incomplete_backward = false,
       is_incomplete_forward = false,
-    })
+    }
     return function() end
   end
 
   local lines = vim.api.nvim_buf_get_lines(ctx.bufnr or 0, 0, -1, false)
 
-  local blink = require("blink.cmp.types")
+  local blink = require 'blink.cmp.types'
   local fields = {
     entry_kind = blink.CompletionItemKind.Reference,
-    documentation_kind = "markdown",
+    documentation_kind = 'markdown',
   }
   local entries = refs.get_entries(lines, fields)
 
@@ -48,11 +48,11 @@ function source:get_completions(ctx, callback)
     items = entries
   end
 
-  callback({
+  callback {
     items = items,
     is_incomplete_backward = false,
     is_incomplete_forward = false,
-  })
+  }
   return function() end
 end
 
--- lua/cmp-pandoc-references/init.lua
+++ lua/cmp-pandoc-references/init.lua
@@ -1,5 +1,5 @@
 local source = {}
-local refs = require("cmp-pandoc-references.references")
+local refs = require 'cmp-pandoc-references.references'
 
 source.new = function()
   return setmetatable({}, { __index = source })
@@ -7,20 +7,20 @@ end
 
 -- Add another filetype if needed
 source.is_available = function()
-  return vim.o.filetype == "pandoc"
-    or vim.o.filetype == "markdown"
-    or vim.o.filetype == "rmd"
-    or vim.o.filetype == "quarto"
+  return vim.o.filetype == 'pandoc'
+    or vim.o.filetype == 'markdown'
+    or vim.o.filetype == 'rmd'
+    or vim.o.filetype == 'quarto'
 end
 
 source.get_keyword_pattern = function()
-  return "[@][^[:blank:]]*"
+  return '[@][^[:blank:]]*'
 end
 
 source.complete = function(self, request, callback)
   local lines = vim.api.nvim_buf_get_lines(self.bufnr or 0, 0, -1, false)
 
-  local cmp = require("cmp")
+  local cmp = require 'cmp'
   local fields = {
     entry_kind = cmp.lsp.CompletionItemKind.Reference,
     documentation_kind = cmp.lsp.MarkupKind.Markdown,
